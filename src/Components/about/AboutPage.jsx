bf04e5d7edf2b971c3fea8510b5f337ac02fc415
Raj Sagar <therajsagar@gmail.com>
2019-04-15 09:53:55 +0530

    first commit

---
+++ src/Components/about/AboutPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const AboutPage= () => (
+    <div className='jumbotron'>
+        <h2>About</h2>
+        <p>This apps uses React, Redux, React Router & many others helpful libraries.</p>
+    </div>
+)
+
+export default AboutPage;
