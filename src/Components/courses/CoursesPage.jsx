31de84f126da1a1ea378a3e758fac70fa8d35c3c
Raj Sagar <therajsagar@gmail.com>
2019-04-21 23:59:57 +0530

    author module added

--- src/Components/courses/CoursesPage.jsx
+++ src/Components/courses/CoursesPage.jsx
@@ -3,19 +3,18 @@ import {connect} from 'react-redux';
 import * as actions from '../../redux/actions/courseActions';
 import PropTypes from 'prop-types';
 import {bindActionCreators} from 'redux';
+import CourseList from './CourseList';
 
 class CoursesPage extends React.Component{
     componentDidMount(){
-        this.props.actions.loadCourses().catch(error => alert("Error"));
+        this.props.actions.loadCourses().catch(error => console.log(error));
     }
 
     render(){
         return (
             <>
                 <h2>Courses</h2>
-                {this.props.courses.map(c => (
-                    <div key={c.title}>{c.title}</div>
-                ))}
+                <CourseList courses={this.props.courses} />
             </>
         );
     }
