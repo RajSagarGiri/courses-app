442040756ac7af3c8d5c6f16a62fc530e1e80477
Raj Sagar <therajsagar@gmail.com>
2019-04-21 22:50:22 +0530

    redux added

--- src/Components/about/AboutPage.jsx
+++ src/Components/about/AboutPage.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 const AboutPage= () => (
     <div className='jumbotron'>
         <h2>About</h2>
         <p>This apps uses React, Redux, React Router & many others helpful libraries.</p>
+        <Link to='/' className='btn btn-primary btn-lg'>Home</Link>
     </div>
 )
 
