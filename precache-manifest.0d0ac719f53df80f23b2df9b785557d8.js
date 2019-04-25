82175e823f983846c326f5b6737541a2bfd95c00
Raj Sagar <therajsagar@gmail.com>
2019-04-25 11:42:13 +0530

    Updates

---
+++ precache-manifest.0d0ac719f53df80f23b2df9b785557d8.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/courses-app/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "e46a47d3d23d56ae2ad5",
+    "url": "/courses-app/static/css/main.cd3014a9.chunk.css"
+  },
+  {
+    "revision": "1f937539e4e8a2191fb3",
+    "url": "/courses-app/static/js/runtime~main.c8436ff8.js"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/courses-app/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "ac766b01dba6acbd7e32",
+    "url": "/courses-app/static/js/2.2d7749ba.chunk.js"
+  },
+  {
+    "revision": "e46a47d3d23d56ae2ad5",
+    "url": "/courses-app/static/js/main.7a663306.chunk.js"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/courses-app/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/courses-app/static/media/glyphicons-halflings-regular.fa277232.woff"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/courses-app/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "ac766b01dba6acbd7e32",
+    "url": "/courses-app/static/css/2.6b7a0635.chunk.css"
+  },
+  {
+    "revision": "5c165200f1934bc510160328d32ab8ef",
+    "url": "/courses-app/index.html"
+  }
+];
